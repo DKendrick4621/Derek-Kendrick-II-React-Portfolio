ea2ec7d251de26fce359d92987859d7ab83ddca1
DEREK KENDRICK <derekkednrick79@gmail.com>
2022-05-18 00:02:53 -0400

    Portfolio Additions

--- src/components/app.js
+++ src/components/app.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
 import moment from 'moment';
 
+import ProtfolioContainer from "./portfolio/portfolio-container"
+
+
 export default class App extends Component {
   render() {
     return (
       <div className='app'>
         <h1>Derek Kendrick II React Portfolio</h1>
         <div>{moment().format("MMMM Do YYYY, h:mm:ss")}</div>
+        <ProtfolioContainer />
        
       </div>
     );
