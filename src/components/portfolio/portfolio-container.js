4a5b85a60c6cf2f8f54f086250dcaa4da1284bdd
DEREK KENDRICK <derekkednrick79@gmail.com>
2022-05-18 23:48:27 -0400

    Added a map feat

--- src/components/portfolio/portfolio-container.js
+++ src/components/portfolio/portfolio-container.js
@@ -4,12 +4,24 @@ import PortfolioItem from "./portfolio-item";
 
 
 export default class App extends Component {
+    constructor(){
+        super();
+        console.log('Portfolio container has rendered');
+        
+    }
+    PortfolioItems() {
+        const data = ["Quip", "Evenbrite", "Ministry Safe"];
+
+        return data.map(item => {
+            return <PortfolioItem title={item} url={"google.com"} />;
+        })
+    }
     render() {
         return (
             <div>
                  <h2>Portfolio items go here...</h2>
 
-                 <PortfolioItem />
+                 {this.PortfolioItems()}
             </div>
         );
     }
