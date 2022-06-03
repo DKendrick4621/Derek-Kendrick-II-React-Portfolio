3e0ce4da38e5e8e0db96d1c4a48b9be9d2759aad
DEREK KENDRICK <derekkednrick79@gmail.com>
2022-06-03 00:21:11 -0400

    working things

--- src/components/navigation/navigation-container.js
+++ src/components/navigation/navigation-container.js
@@ -9,24 +9,39 @@ export default class NavigationContainer extends Component {
 
 render() {
     return (
-        <div>
-            <NavLink exact to="/">
+        <div className="nav-wrapper">
+            <div className="left-side">
+
+                <div className="nav-link-wrapper">
+            <NavLink exact to="/" activeClassName="nav-link-active">
                 Home
             </NavLink>
+            </div>
 
+            <div className="nav-link-wrapper">
             <NavLink to="/about-me" activeClassName="nav-link-active">
                 About
             </NavLink>
+            </div>
 
+            <div className="nav-link-wrapper">
             <NavLink exact to="/contact" activeClassName="nav-link-active">
                 Contact
             </NavLink>
+            </div>
 
+            <div className="nav-link-wrapper">
             <NavLink exact to="/blog" activeClassName="nav-link-active">
                 Blog
             </NavLink>
+            </div>
 
-           {false ? <button>Add Blog</button> : null }
+           </div>
+
+
+           <div className="right-side">
+               DEREK L KENDRICK II
+           </div>
         </div>
     )
 }
